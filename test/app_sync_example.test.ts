9d63c545979dfe98839055897f18543034b772b9
zqumei <zaid.qumei+os@gmail.com>
2023-09-27 16:14:40 -0400

    Initial commit

---
+++ test/app_sync_example.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AppSyncExample from '../lib/app_sync_example-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/app_sync_example-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AppSyncExample.AppSyncExampleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
